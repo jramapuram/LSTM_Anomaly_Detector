24b3569d28f7745ad3dd0bf9d1922329dda9a500
Jason Ramapuram <jason.ramapuram@viasat.com>
2015-05-06 10:41:36 +0200

    proper saving and loading

--- config.py
+++ config.py
@@ -2,10 +2,11 @@ __author__ = 'jramapuram'
 
 class config():
     def __init__(self, num_periods=128, num_test_periods=32, max_features=20000
-                 , input_dim=512, hidden_dim=256
+                 , input_dim=512, hidden_dim=256, batch_size=16
                  , activation='sigmoid', inner_activation='hard_sigmoid'
                  , initialization='glorot_uniform'
-                 , optimizer='rmsprop', loss='mean_squared_error'
+                 , model_file='weights_%din_%dhid_%dbatch_%depochs.dat'
+                 , optimizer='rmsprop', loss='mean_squared_error', max_epochs=30
                  , truncate_gradient=-1, return_sequences=False):
         self.num_periods = num_periods
         self.num_test_periods = num_test_periods
@@ -14,12 +15,15 @@ class config():
         self.hidden_dim = hidden_dim
         self.truncate_gradient = truncate_gradient
         self.return_sequences = return_sequences
+        self.model_file = 'weights_%din_%dhid_%dbatch_%depochs.dat'
         # options: 'glorot_normal', 'glorot_uniform', 'normal', 'he_normal', 'he_uniform'
         self.initialization = initialization
         self.inner_init = inner_activation
         self.activation = activation
         self.optimizer = optimizer
         self.loss = loss
+        self.max_epochs = max_epochs
+        self.batch_size = batch_size
 
     def get_config(self):
         return {"name":self.__class__.__name__,
@@ -33,5 +37,8 @@ class config():
             "activation":self.activation,
             "optimizer":self.optimizer,
             "loss":self.loss,
+            "model_file":self.model_file,
+            "batch_size":self.batch_size,
+            "max_epochs":self.max_epochs,
             "truncate_gradient":self.truncate_gradient,
             "return_sequences":self.return_sequences}
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 __author__ = 'jramapuram'
+import os.path
 import numpy as np
 from config import config
 from math import sin, pi
@@ -21,8 +22,8 @@ def build_autoencoder(conf):
     model.compile(loss=conf['loss'], optimizer=conf['optimizer'])
     return model
 
-def generate_sin_wave(input_size, num_waves):
-    delta = 2*pi/(input_size-1)  # for proper shifting
+def generate_sin_wave(input_size, num_waves, offset=-1):
+    delta = 2*pi/(input_size-offset)  # for proper shifting
     one_wave = [sin(delta*i) for i in xrange(0, input_size)]
     return one_wave*num_waves
 
@@ -33,13 +34,39 @@ def test_sin_wave(conf):
     plt.plot(generate_sin_wave(conf['input_dim'], conf['num_periods']))
     plt.show()
 
+def load_model(path_str, model):
+    if os.path.isfile(path_str):
+        print 'model found, loading existing model...'
+        model.load_weights(path_str)
+        return True
+    else:
+        print 'model does not exist...'
+        return False
+
 if __name__ == "__main__":
     conf = config().get_config()
+
+    print 'generating data....'
     X_train = split_vector(generate_sin_wave(conf['input_dim'], conf['num_periods']), conf['input_dim'])
     print 'X_train size: %s | input_size: %d' % (X_train.shape, conf['input_dim'])
     X_test = split_vector(generate_sin_wave(conf['input_dim'], conf['num_test_periods']), conf['input_dim'])
     print 'X_test size: %s | input_size: %d' % (X_test.shape, conf['input_dim'])
 
+    print 'building lstm autoencoder...'
     model = build_autoencoder(conf)
-    model.fit(X_train, X_train, batch_size=16, nb_epoch=100, validation_split=0.1, show_accuracy=True)
-    score = model.evaluate(X_test, X_test, batch_size=16)
+    model_name = conf['model_file'] % (conf['input_dim'], conf['hidden_dim'], conf['batch_size'], conf['max_epochs'])
+    model_exists = load_model(model_name, model)
+
+    if not model_exists:
+        print 'training new model...'
+        model.fit(X_train, X_train
+                  , batch_size=conf['batch_size']
+                  , nb_epoch=conf['max_epochs']
+                  , validation_split=0.1
+                  , show_accuracy=True)
+        print 'saving model to %s...' % model_name
+        model.save_weights(model_name)
+
+    score = model.evaluate(X_test, X_test, batch_size=conf['batch_size'], show_accuracy=True)
+    print 'model score on test set: ', score
+
