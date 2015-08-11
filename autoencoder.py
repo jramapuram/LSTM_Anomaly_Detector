700ae0379ea080f676c83fbebf0a1514c15d4b23
Jason Ramapuram <jason.ramapuram@viasat.com>
2015-08-11 10:45:44 +0200

    not working yet

--- autoencoder.py
+++ autoencoder.py
@@ -1,15 +1,17 @@
 __author__ = 'jramapuram'
 
+import math
 import os.path
-
 import numpy as np
+import numpy as np
+import six
 
+import chainer
+import numpy as np
+import chainer.functions as F
+
+from chainer import cuda, Function, FunctionSet, gradient_check, Variable, optimizers
 from data_manipulator import elementwise_square
-from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, AutoEncoder, Activation
-from keras.layers.recurrent import LSTM, GRU
-from keras.regularizers import l2
-from convolutional import Convolution1D, MaxPooling1D
 
 
 class TimeDistributedAutoEncoder:
@@ -19,14 +21,14 @@ class TimeDistributedAutoEncoder:
         self.model_name = ''
         self.encoder_sizes = []
         self.decoder_sizes = []
-        self.models = []
-        self.compiled = False
+        self.encoders = {}
+        self.decoders = {}
+        self.model = None
+        self.state = None
+        self.optimizer = optimizers.Adam()  # Parametrize
 
     @property
     def get_model_name(self):
-        if not self.compiled:
-            raise Exception("Cannot determine model name without it first being compiled");
-
         model_structure = 'weights_[%s]Enc_[%s]Dec_%dbatch_%s_autoencoder.dat'
         model_name = model_structure % ('_'.join(str(e) for e in self.encoder_sizes)
                                         , '_'.join(str(d) for d in self.decoder_sizes)
@@ -37,79 +39,70 @@ class TimeDistributedAutoEncoder:
         create_dir(model_dir)
         return model_dir, model_name
 
-    def compile(self, optimizer=None):
-        for model in self.models:
-            print model.get_config(verbose=1)
-            if optimizer is not None:
-                model.compile(loss=self.conf['--loss'], optimizer=optimizer)
-            else:
-                model.compile(loss=self.conf['--loss'], optimizer=self.conf['--optimizer'])
-        self.compiled = True
+    def make_initial_state(self, batch_size, train=True):
+        state = {}
+        for i in range(0, len(self.encoder_sizes)):
+            state['c_e' + str(i)] = chainer.Variable(cuda.zeros((batch_size, self.encoder_sizes[i]),
+                                                                dtype=np.float32),
+                                                     volatile=not train)
+            state['h_e' + str(i)] = chainer.Variable(cuda.zeros((batch_size, self.encoder_sizes[i]),
+                                                                dtype=np.float32),
+                                                     volatile=not train)
+        for j in range(0, len(self.decoder_sizes)):
+            state['c_d' + str(i)] = chainer.Variable(cuda.zeros((batch_size, self.decoder_sizes[i]),
+                                                                dtype=np.float32),
+                                                     volatile=not train)
+            state['h_d' + str(i)] = chainer.Variable(cuda.zeros((batch_size, self.decoder_sizes[i]),
+                                                                dtype=np.float32),
+                                                     volatile=not train)
+        return state
 
-    def add_autoencoder(self, encoder_sizes=[], decoder_sizes=[]):
+    def forward_one_step_lstm(self, x_data, y_data, state, train=True):
+        x_data = cuda.to_gpu(x_data)
+        y_data = cuda.to_gpu(y_data)
+        x = chainer.Variable(x_data, volatile=not train)
+        t = chainer.Variable(y_data, volatile=not train)
+
+        # Encoder section : Special case for 0 as it uses raw data x
+        state['h_e_in0'] = self.encoders['le_x0'](F.dropout(x, train=train)) + self.encoders['le_h0'](state['h_e0'])
+        print state['h_e_in0'].__len__()
+        for i in range(0, len(self.encoder_sizes) - 1):
+            num = str(i)
+            num_plus_one = str(i + 1)
+            state['c_e' + num], state['h_e' + num] = F.lstm(state['c_e' + num], state['h_e_in' + num])
+            state['h_e_in' + num_plus_one] = self.encoders['le_x' + num_plus_one](F.dropout('h_e' + num, train=train)) \
+                                             + self.encoders['e_h' + num_plus_one](state['h_e' + num_plus_one])
+
+        # Decoder section: Special case for 0 as it uses last encoder state
+        state['c_d0'], state['h_d0'] = F.lstm(state['c_d0'], state['h_e_in' + str(i + 1)])
+        for i in range(0, len(self.decoder_sizes) - 2):
+            num = str(i)
+            state['h_d_in' + num] = self.decoders['ld_x' + num](F.dropout('h_d' + num, train=train)) \
+                                    + self.decoders['ld_h' + num](state['h_d' + num])
+            state['c_d' + num], state['h_d' + num] = F.lstm(state['c_d' + num], state['h_d_in' + num])
+
+        y = self.decoders['ld_h' + str(i + i)](F.dropout(state['h_d' + str(i)], train=train))
+
+        return state, F.mean_squared_error(y, t)
+
+    def add_autoencoder(self, encoder_sizes, decoder_sizes):
         assert(len(encoder_sizes) != 0 and len(decoder_sizes) != 0)
         assert(len(encoder_sizes) == len(decoder_sizes))
 
         self.encoder_sizes = encoder_sizes
         self.decoder_sizes = decoder_sizes
-        # self.models = [Sequential() for i in range(len(encoder_sizes))]
-        self.models = [Sequential()]
 
-        encoders = Sequential()
-        decoders = Sequential()
         for i in range(0, len(encoder_sizes) - 1):
-            encoders.add(Dense(encoder_sizes[i], encoder_sizes[i + 1]
-                               , init=self.conf['--initialization']
-                               , activation=self.conf['--activation']
-                               , W_regularizer=l2()))
+            self.encoders['e' + str(i)] = F.Linear(encoder_sizes[i], encoder_sizes[i + 1])
+            self.decoders['d' + str(i)] = F.Linear(decoder_sizes[i], decoder_sizes[i + 1])
+        layers = self.encoders.copy()
+        self.model = FunctionSet(layers.update(self.decoders))
+        for param in self.model.parameters:
+            param[:] = np.random.uniform(-0.1, 0.1, param.shape)
+        cuda.init()
+        self.model.to_gpu()
 
-            decoders.add(Dense(decoder_sizes[i], decoder_sizes[i + 1]
-                               , init=self.conf['--initialization']
-                               , activation=self.conf['--activation']
-                               , W_regularizer=l2()))
-
-        self.models[0].add(AutoEncoder(encoder=encoders
-                                       , decoder=decoders
-                                       , tie_weights=True
-                                       , output_reconstruction=(i == 0)))
-        return self.models
-
-    # TODO: This doesnt work yet
-    # (batch size, stack size, nb row, nb col)
-    def add_conv_autoencoder(self, encoder_sizes=[], decoder_sizes=[]):
-        assert(len(encoder_sizes) != 0 and len(decoder_sizes) != 0)
-        assert(len(encoder_sizes) == len(decoder_sizes))
-
-        self.encoder_sizes = encoder_sizes
-        self.decoder_sizes = decoder_sizes
-        # self.models = [Sequential() for i in range(len(encoder_sizes))]
-        self.models = [Sequential()]
-
-        encoders = Sequential()
-        decoders = Sequential()
-        for i in range(0, len(encoder_sizes) - 1):
-            encoders.add(Convolution1D(32, 3, 3
-                                       , activation=self.conf['--activation']
-                                       , init=self.conf['--initialization']
-                                       , border_mode='valid'))
-            encoders.add(Activation('relu'))
-            encoders.add(MaxPooling1D())
-            encoders.add(Convolution1D(32, 1, 1
-                                       , activation=self.conf['--activation']
-                                       , init=self.conf['--initialization']
-                                       , border_mode='valid'))
-
-            decoders.add(Convolution1D(32, 1, 1
-                                       , activation=self.conf['--activation']
-                                       , init=self.conf['--initialization']
-                                       , border_mode='valid'))
-            decoders.add(Activation('relu'))
-            decoders.add(MaxPooling1D())
-
-        self.models[0].add(AutoEncoder(encoder=encoders
-                                       , decoder=decoders
-                                       , output_reconstruction=(i == 0)))
-        return self.models
+        return self.model
 
     def add_lstm_autoencoder(self, encoder_sizes=[], decoder_sizes=[]):
         assert(len(encoder_sizes) != 0 and len(decoder_sizes) != 0)
@@ -117,31 +110,22 @@ class TimeDistributedAutoEncoder:
 
         self.encoder_sizes = encoder_sizes
         self.decoder_sizes = decoder_sizes
-        # self.models = [Sequential() for i in range(len(encoder_sizes))]
-        self.models = [Sequential()]
 
-        encoders = Sequential()
-        decoders = Sequential()
-        for i in range(0, len(encoder_sizes) - 1):
-            encoders.add(LSTM(encoder_sizes[i], encoder_sizes[i + 1]
-                              , activation=self.conf['--activation']
-                              , inner_activation=self.conf['--inner_activation']
-                              , init=self.conf['--initialization']
-                              , inner_init=self.conf['--inner_init']
-                              , truncate_gradient=int(self.conf['--truncated_gradient'])
-                              , return_sequences=True))
-            decoders.add(LSTM(decoder_sizes[i], decoder_sizes[i + 1]
-                              , activation=self.conf['--activation']
-                              , inner_activation=self.conf['--inner_activation']
-                              , init=self.conf['--initialization']
-                              , inner_init=self.conf['--inner_init']
-                              , truncate_gradient=int(int(self.conf['--truncated_gradient']))
-                              , return_sequences=not (i == len(encoder_sizes) - 1)))
+        for i in range(0, len(encoder_sizes)):
+            self.encoders['le_x' + str(i)] = F.Linear(encoder_sizes[i], 4 * encoder_sizes[i])
+            self.encoders['le_h' + str(i)] = F.Linear(encoder_sizes[i], 4 * encoder_sizes[i])
+            self.decoders['ld_x' + str(i)] = F.Linear(decoder_sizes[i], 4 * decoder_sizes[i])
+            self.decoders['ld_h' + str(i)] = F.Linear(decoder_sizes[i], 4 * decoder_sizes[i])
+        layers = self.encoders.copy()
+        layers.update(self.decoders)
 
-        self.models[0].add(AutoEncoder(encoder=encoders
-                                       , decoder=decoders
-                                       , output_reconstruction=(i == 0)))
-        return self.models
+        self.model = FunctionSet(**layers)
+        for param in self.model.parameters:
+            param[:] = np.random.uniform(-0.1, 0.1, param.shape)
+        cuda.init()
+        self.model.to_gpu()
+        self.state = self.make_initial_state(int(self.conf['--batch_size']), train=True)
+        return self.model
 
     def format_lstm_data(self, x):
         # Need to create a 3d vector [samples, timesteps, input_dim]
@@ -166,28 +150,39 @@ class TimeDistributedAutoEncoder:
     def sigmoid(x):
         return 1 / (1 + np.exp(-x))
 
+    def evaluate_lstm(self, dataset):
+        sum_log_perp = cuda.zeros(())
+        state = self.make_initial_state(batch_size=1, train=False)
+        for i in six.moves.range(dataset.size - 1):
+            x_batch = dataset[i:i + 1]
+            y_batch = dataset[i + 1:i + 2]
+            state, loss = self.forward_one_step_lstm(x_batch, y_batch, state, train=False)
+            sum_log_perp += loss.data.reshape(())
+
+        return math.exp(cuda.to_cpu(sum_log_perp) / (dataset.size - 1))
+
     def train_and_predict(self, x):
-        self.compile()
-
         self.model_dir, self.model_name = self.get_model_name
-        _ = self.load_model(os.path.join(self.model_dir, self.model_name), self.models[0])
-
-        x = self.format_lstm_data(x)
-
+        #_ = self.load_model(os.path.join(self.model_dir, self.model_name), self.models[0])
+        # x = self.format_lstm_data(x)
+        x = x.astype(np.float32)
         predictions = []
+        self.state, loss = self.forward_one_step_lstm(x[0:1, :], x[0:1, :], self.state, train=True)
         for i in xrange(x.shape[0] - 1):
             if self.conf['--model_type'].strip().lower() == 'lstm':
-                predictions.append(self.models[0].predict(x[i:i+1, :, :], verbose=False))
-                self.models[0].train_on_batch(x[i:i+1, :, :], x[i:i+1, :, :], accuracy=True)
+                current_data = np.array([x[i:i+1, :], x[i:i+1, :]])
+                predictions.append(self.evaluate_lstm(current_data))
+                self.state, loss = self.forward_one_step_lstm(x[i:i+1, :], x[i:i+1, :], self.state, train=True)
             else:
                 predictions.append(self.models[0].predict(x[i:i+1, :], verbose=False))
-                self.models[0].train_on_batch(x[i:i+1, :], x[i:i+1, :], accuracy=True)
+                self.models.train_on_batch(x[i:i+1, :], x[i:i+1, :], accuracy=True)
 
         predictions.append(predictions[-1])  # XXX
         print 'saving model to %s...' % os.path.join(self.model_dir, self.model_name)
-        self.models[0].save_weights(os.path.join(self.model_dir, self.model_name), overwrite=True)
+        #self.models[0].save_weights(os.path.join(self.model_dir, self.model_name), overwrite=True)
 
-        predictions = np.squeeze(np.squeeze(np.array(predictions), (1,)), (1,))
+        #predictions = np.squeeze(np.squeeze(np.array(predictions), (1,)), (1,))
+        predictions = np.array(predictions)
         print 'predictions.shape: ', predictions.shape
         np.savetxt(os.path.join(self.model_dir, 'outputs.csv'), predictions, delimiter=',')
         return predictions
